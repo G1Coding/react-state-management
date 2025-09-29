0a4b47a1c1f98aa2a8a0ce2de22b1b5eb2a0ce88
G1Coding <32190250@dankook.ac.kr>
2025-09-30 05:05:39 +0900

    chore: React StrictMode 해제

--- React-ContextAPI/client/src/index.js
+++ React-ContextAPI/client/src/index.js
@@ -6,11 +6,11 @@ import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <OrderContextProvider>
       <App />
     </OrderContextProvider>
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
 reportWebVitals();

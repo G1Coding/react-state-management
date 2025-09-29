c4db894a5a4e29bb37bf2c33b2abb15435349edd
G1Coding <32190250@dankook.ac.kr>
2025-09-30 04:55:27 +0900

    feat: 상품 수량 변경 시 context 상태 updateItemCount로 반영

--- React-ContextAPI/client/src/components/Products.js
+++ React-ContextAPI/client/src/components/Products.js
@@ -1,7 +1,12 @@
 import React from "react";
 
-const Products = ({ name, imagePath }) => {
-  console.log(name, imagePath);
+const Products = ({ name, imagePath, updateItemCount }) => {
+
+  const handleChange = (event) => {
+    const currentValue = event.target.value;
+    updateItemCount(name, currentValue);
+  }
+
   return (
     <div style={{ textAlign: "center" }}>
       <img
@@ -18,6 +23,7 @@ const Products = ({ name, imagePath }) => {
           name="quentity"
           min="0"
           defaultValue="0"
+          onChange={handleChange}
         />
       </form>
     </div>

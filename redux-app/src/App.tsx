670764a054ad8ea20f6b5e6a551af1d444a4a58f
G1Coding <32190250@dankook.ac.kr>
2025-10-02 16:49:22 +0900

    feat: redux-thunk 적용 및 posts 비동기 데이터 fetch 기능 추가

--- redux-app/src/App.tsx
+++ redux-app/src/App.tsx
@@ -1,15 +1,17 @@
-import React, { useState } from 'react';
-import './App.css';
-import { useDispatch, useSelector } from 'react-redux';
-import { RootState } from './reducer';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "./reducer";
+import axios from "axios";
+import { Post } from "./reducer/posts";
 
 type Props = {
   value: any;
-  onIncrement : () => void;
-  onDecrement : () => void;
-}
+  onIncrement: () => void;
+  onDecrement: () => void;
+};
 
-function App({value, onIncrement, onDecrement} : Props) {
+function App({ value, onIncrement, onDecrement }: Props) {
   const counter = useSelector((state: RootState) => state.counter);
   const todos: string[] = useSelector((state: RootState) => state.todos);
 
@@ -17,30 +19,51 @@ function App({value, onIncrement, onDecrement} : Props) {
   const dispatch = useDispatch();
 
   const [todoValue, setTodoValue] = useState("d");
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) =>{
-    setTodoValue(e.target.value)
-  }
+  const posts: Post[] = useSelector((state: RootState) => state.posts);
+
+  useEffect(() => {
+    dispatch(fetchPosts());
+  }, [dispatch]);
+
+  const fetchPosts = (): any => {
+    return async function fetchPostsThunk(dispatch: any, getState: any) {
+      const response = await axios.get(
+        `${process.env.REACT_APP_THUNK_API_URL}/posts`
+      );
+      dispatch({ type: "FETCH_POSTS", payload: response.data });
+    };
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTodoValue(e.target.value);
+  };
   const addTodo = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    dispatch({type: "ADD_TODO", text: todoValue})
+    dispatch({ type: "ADD_TODO", text: todoValue });
     setTodoValue("");
-  }
+  };
 
-  return ( 
+  return (
     <div className="App">
       Clicked: {counter} times
       <button onClick={onIncrement}>+</button>
       <button onClick={onDecrement}>-</button>
-
-    {/* 할 일 목록 렌더링 */}
-    <ul>
-      {todos.map((todo, index) => <li key={index}>{todo}</li>)}
-    </ul>
-
-      <form onSubmit={addTodo} >
-        <input type='text' value={todoValue} onChange={handleChange} />
-        <input type='submit' />
+      {/* 할 일 목록 렌더링 */}
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>{todo}</li>
+        ))}
+      </ul>
+      <form onSubmit={addTodo}>
+        <input type="text" value={todoValue} onChange={handleChange} />
+        <input type="submit" />
       </form>
+      {/* posts 한 데이터 렌더링 */}
+      <ul>
+        {posts.map((post, index) => (
+          <li key={index}>{`${post.title}, ${post.id}`} </li>
+        ))}
+      </ul>
     </div>
   );
 }

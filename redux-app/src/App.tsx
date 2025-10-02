46c7963533a1f32ddd1a50ec1a8b53862f9dcf75
G1Coding <32190250@dankook.ac.kr>
2025-10-02 10:54:13 +0900

    feat: useSelector로 store에 있는 state 값 가져옴

--- redux-app/src/App.tsx
+++ redux-app/src/App.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import { useSelector } from 'react-redux';
+import { RootState } from './reducer';
 
 type Props = {
   value: any;
@@ -8,11 +10,32 @@ type Props = {
 }
 
 function App({value, onIncrement, onDecrement} : Props) {
+  const counter = useSelector((state: RootState) => state.counter);
+  const todos: string[] = useSelector((state: RootState) => state.todos)
+  const [todoValue, setTodoValue] = useState("d");
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) =>{
+    setTodoValue(e.target.value)
+  }
+  const addTodo = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setTodoValue("");
+  }
+
   return ( 
     <div className="App">
-      {/* Clicked: {value} times */}
+      Clicked: {counter} times
       <button onClick={onIncrement}>+</button>
       <button onClick={onDecrement}>-</button>
+
+    {/* 할 일 목록 렌더링 */}
+    <ul>
+      {todos.map((todo, index) => <li key={index}>{todo}</li>)}
+    </ul>
+
+      <form onSubmit={addTodo} >
+        <input type='text' value={todoValue} onChange={handleChange} />
+        <input type='submit' />
+      </form>
     </div>
   );
 }
